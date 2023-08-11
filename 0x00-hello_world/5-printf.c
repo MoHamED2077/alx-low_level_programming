54e4a6107eed4ed6f3caf3b1ed74e28bb8cd88fb
Your Name <you@example.com>
2023-08-10 18:03:05 -0700

    jdhs

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+*main - entrypoint
+*
+*Description: A C program with prientf function
+*
+*Return: Always 0 (success)
+*/
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
